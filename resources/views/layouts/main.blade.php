670a850a09a53ebcac5e3add9f4005c64c274ffc
Devpph <52081212+thomaswinchestar@users.noreply.github.com>
2022-07-27 09:11:32 +0630

    app,main,post

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Material Design for Bootstrap</title>
+    <title>{{ config('app.name', 'ElBlogCMS') }}</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
     <!-- Google Fonts Roboto -->
@@ -38,7 +38,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
             <div class="container-fluid">
                 <!-- Navbar brand -->
-                <a class="navbar-brand" target="_blank" href="https://mdbootstrap.com/docs/standard/">
+                <a class="navbar-brand" href="/">
                     <img src="/img/logo.svg" height="50" alt="" loading="lazy" style="margin-top: -3px;" />
                 </a>
                 <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
@@ -62,19 +62,13 @@
                     <ul class="navbar-nav d-flex flex-row">
                         <!-- Icons -->
                         <li class="nav-item me-3 me-lg-0">
-                            <a class="nav-link" href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA"
-                                rel="nofollow" target="_blank">
-                                <i class="fab fa-youtube"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item me-3 me-lg-0">
-                            <a class="nav-link" href="https://www.facebook.com/mdbootstrap" rel="nofollow"
+                            <a class="nav-link" href="https://www.facebook.com/thomas.winchestar" rel="nofollow"
                                 target="_blank">
                                 <i class="fab fa-facebook-f"></i>
                             </a>
                         </li>
                         <li class="nav-item me-3 me-lg-0">
-                            <a class="nav-link" href="https://twitter.com/MDBootstrap" rel="nofollow"
+                            <a class="nav-link" href="https://twitter.com/thomaswinches11" rel="nofollow"
                                 target="_blank">
                                 <i class="fab fa-twitter"></i>
                             </a>
@@ -97,12 +91,12 @@
 
         <!-- Jumbotron -->
         <div id="intro" class="p-5 text-center bg-light">
-            <h1 class="mb-3 h2">Learn Web Development</h1>
-            <p class="mb-3">Best & free guide of responsive web design</p>
-            <a class="btn btn-primary m-2" href="https://www.youtube.com/watch?v=c9B4TPnak1A" role="button"
-                rel="nofollow" target="_blank">Start tutorial</a>
-            <a class="btn btn-primary m-2" href="https://mdbootstrap.com/docs/standard/" target="_blank"
-                role="button">Download MDB UI KIT</a>
+            <h1 class="mb-3 h2">El Blog CMS</h1>
+            <p class="mb-3">Sharing is caring!</p>
+            <a class="btn btn-primary m-2" href="{{ route('login') }}" role="button"
+                rel="nofollow" target="_blank">Login</a>
+            <a class="btn btn-primary m-2" href="{{ route('register') }}" target="_blank"
+                role="button">Register</a>
         </div>
         <!-- Jumbotron -->
     </header>
@@ -142,34 +136,30 @@
 
     <!--Footer-->
     <footer class="bg-light text-lg-start">
-        <div class="py-4 text-center">
-            <a role="button" class="btn btn-primary btn-lg m-2"
-                href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow" target="_blank">
-                Learn Bootstrap 5
-            </a>
-            <a role="button" class="btn btn-primary btn-lg m-2" href="https://mdbootstrap.com/docs/standard/"
-                target="_blank">
-                Download MDB UI KIT
-            </a>
-        </div>
+{{--        <div class="py-4 text-center">--}}
+{{--            <a role="button" class="btn btn-primary btn-lg m-2"--}}
+{{--                href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow" target="_blank">--}}
+{{--                Learn Bootstrap 5--}}
+{{--            </a>--}}
+{{--            <a role="button" class="btn btn-primary btn-lg m-2" href="https://mdbootstrap.com/docs/standard/"--}}
+{{--                target="_blank">--}}
+{{--                Download MDB UI KIT--}}
+{{--            </a>--}}
+{{--        </div>--}}
 
         <hr class="m-0" />
 
         <div class="text-center py-4 align-items-center">
-            <p>Follow MDB on social media</p>
-            <a href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" class="btn btn-primary m-1"
-                role="button" rel="nofollow" target="_blank">
-                <i class="fab fa-youtube"></i>
-            </a>
-            <a href="https://www.facebook.com/mdbootstrap" class="btn btn-primary m-1" role="button" rel="nofollow"
+            <p>Follow me on social platform!</p>
+            <a href="https://www.facebook.com/thomas.winchestar" class="btn btn-primary m-1" role="button" rel="nofollow"
                 target="_blank">
                 <i class="fab fa-facebook-f"></i>
             </a>
-            <a href="https://twitter.com/MDBootstrap" class="btn btn-primary m-1" role="button" rel="nofollow"
+            <a href="https://twitter.com/thomaswinches11" class="btn btn-primary m-1" role="button" rel="nofollow"
                 target="_blank">
                 <i class="fab fa-twitter"></i>
             </a>
-            <a href="https://github.com/mdbootstrap/mdb-ui-kit" class="btn btn-primary m-1" role="button"
+            <a href="https://github.com/thomaswinchestar" class="btn btn-primary m-1" role="button"
                 rel="nofollow" target="_blank">
                 <i class="fab fa-github"></i>
             </a>
@@ -177,8 +167,8 @@
 
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
-            <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            © 2022
+            <a class="text-dark" href="https://devpph.netlify.app/">Devpph</a>
         </div>
         <!-- Copyright -->
     </footer>
